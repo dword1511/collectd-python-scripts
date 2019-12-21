f37bf2ba8e0eb2180e1e67f0dfa74eee4afdc8a1
dword1511 <zhangchi866@gmail.com>
2019-12-20 23:09:20 -0800

    python3 nvmecli, namespace utilization, and code cleanup

--- nvmecli.py
+++ nvmecli.py
@@ -10,11 +10,9 @@ import os, subprocess
 import re, json
 
 
-# TODO: consider 'smart_powercycles'
 def emit_count(vl, a, v):
   vl.dispatch(type = 'count', type_instance = a, values = [v])
 
-# TODO: consider 'smart_badsectors'
 def emit_error(vl, a, v):
   vl.dispatch(type = 'disk_error', type_instance = a, values = [v])
 
@@ -25,12 +23,11 @@ def emit_percentage(vl, a, v):
   vl.dispatch(type = 'percent', type_instance = a, values = [v])
 
 # Will be converted to ops per interval
-# NOTE: the 'command' type is relatively new
+# NOTE: the 'command' type is relatively new, so not using it
 def emit_commands(vl, a, v):
   vl.dispatch(type = 'operations', type_instance = a, values = [v])
 
 # Unit: Kelvin (Celsius + 273.15)
-# TODO: consider 'smart_temperature'
 def emit_temperature(vl, a, v):
   vl.dispatch(type = 'temperature', type_instance = a, values = [v - 273])
 
@@ -40,102 +37,76 @@ def emit_duration_seconds(vl, a, v):
 def emit_duration_minutes(vl, a, v):
   vl.dispatch(type = 'duration', type_instance = a, values = [v * 60])
 
-# TODO: consider 'smart_poweron'
 def emit_uptime_minutes(vl, a, v):
   vl.dispatch(type = 'uptime', type_instance = a, values = [v * 60])
 
-# TODO: consider 'smart_poweron'
 def emit_uptime_hours(vl, a, v):
   vl.dispatch(type = 'uptime', type_instance = a, values = [v * 3600])
 
 # Unit: 1000 * 512, regardless of NVM's LBA size
-# TODO: 'disk_octets' type require read and write at the same time
 def emit_bytes(vl, a, v):
   vl.dispatch(type = 'total_bytes', type_instance = a, values = [v * 512000])
 
-def process_cmd(vl, dev, cmd):
-  funcs = {
-    # See NVME 1.3b Fig. 93.
-    'critical_warning'      : emit_gauge,           # Currently not parsing bit-fields
-    'temperature'           : emit_temperature,     # Device composite temperature
-    'avail_spare'           : emit_percentage,
-    'spare_thresh'          : emit_percentage,
-    'percent_used'          : emit_percentage,      # Used endurance
-    'data_units_read'       : emit_bytes,
-    'data_units_written'    : emit_bytes,
-    'host_read_commands'    : emit_commands,        # Too large to be drawn with other counts
-    'host_write_commands'   : emit_commands,        # Too large to be drawn with other counts
-    'controller_busy_time'  : emit_uptime_minutes,  # Put it together with power on hours
-    'power_cycles'          : emit_count,
-    'power_on_hours'        : emit_uptime_hours,
-    'unsafe_shutdowns'      : emit_count,           # Put it together with power cycles
-    'media_errors'          : emit_error,           # Uncorrectable events
-    'num_err_log_entries'   : emit_error,
-    'warning_temp_time'     : emit_duration_minutes,
-    'critical_comp_time'    : emit_duration_minutes,
-    'temperature_sensor_1'  : emit_temperature,     # Not always present
-    'temperature_sensor_2'  : emit_temperature,     # Not always present
-    'temperature_sensor_3'  : emit_temperature,     # Not always present
-    'temperature_sensor_4'  : emit_temperature,     # Not always present
-    'temperature_sensor_5'  : emit_temperature,     # Not always present
-    'temperature_sensor_6'  : emit_temperature,     # Not always present
-    'temperature_sensor_7'  : emit_temperature,     # Not always present
-    'temperature_sensor_8'  : emit_temperature,     # Not always present
-    'thm_temp1_trans_count' : emit_error,
-    'thm_temp2_trans_count' : emit_error,
-    'thm_temp1_total_time'  : emit_duration_seconds,
-    'thm_temp2_total_time'  : emit_duration_seconds,
-    'wctemp'                : emit_temperature,     # From controller ID
-    'cctemp'                : emit_temperature,     # From controller ID
-  }
+# Namespace usage
+# Unit: 512, regardless of NVM's LBA size
+def emit_df(vl, a, v):
+  vl.dispatch(type = 'df_complex', type_instance = a, values = [v * 512])
 
-  names = {
-    'critical_warning'      : 'Warning Flag',
-    'temperature'           : 'Composite',
-    'avail_spare'           : 'Avail Spares',
-    'spare_thresh'          : 'Spare Limit',
-    'percent_used'          : 'Used Endurance',
-    'data_units_read'       : 'Read bps',
-    'data_units_written'    : 'Write bps',
-    'host_read_commands'    : 'Read ops',
-    'host_write_commands'   : 'Write ops',
-    'controller_busy_time'  : 'Ctl Busy',
-    'power_cycles'          : 'Power Cycles',
-    'power_on_hours'        : 'Total',
-    'unsafe_shutdowns'      : 'Unsafe Shutdown',
-    'media_errors'          : 'Media Errors',
-    'num_err_log_entries'   : 'Error Logs',
-    'warning_temp_time'     : 'Warning Temp',
-    'critical_comp_time'    : 'Critical Temp',
-    'temperature_sensor_1'  : 'Sensor 1',
-    'temperature_sensor_2'  : 'Sensor 2',
-    'temperature_sensor_3'  : 'Sensor 3',
-    'temperature_sensor_4'  : 'Sensor 4',
-    'temperature_sensor_5'  : 'Sensor 5',
-    'temperature_sensor_6'  : 'Sensor 6',
-    'temperature_sensor_7'  : 'Sensor 7',
-    'temperature_sensor_8'  : 'Sensor 8',
-    'thm_temp1_trans_count' : 'Thermal Limit 1',
-    'thm_temp2_trans_count' : 'Thermal Limit 2',
-    'thm_temp1_total_time'  : 'Thermal Limit 1',
-    'thm_temp2_total_time'  : 'Thermal Limit 2',
-    'wctemp'                : 'Warning Composite',
-    'cctemp'                : 'Critical Composite',
+def process_cmd(vl, dev, cmd):
+  properties = {
+    # See NVME 1.3b Fig. 93.
+    'critical_warning'      : ('Warning Flag'      , emit_gauge           ), # Currently not parsing bit-fields
+    'temperature'           : ('Composite'         , emit_temperature     ), # Device composite temperature
+    'avail_spare'           : ('Avail Spares'      , emit_percentage      ),
+    'spare_thresh'          : ('Spare Limit'       , emit_percentage      ),
+    'percent_used'          : ('Used Endurance'    , emit_percentage      ),
+    'data_units_read'       : ('Read bps'          , emit_bytes           ),
+    'data_units_written'    : ('Write bps'         , emit_bytes           ),
+    'host_read_commands'    : ('Read ops'          , emit_commands        ),
+    'host_write_commands'   : ('Write ops'         , emit_commands        ),
+    'controller_busy_time'  : ('Ctl Busy'          , emit_uptime_minutes  ), # Put it together with power on hours
+    'power_cycles'          : ('Power Cycles'      , emit_count           ),
+    'power_on_hours'        : ('Total'             , emit_uptime_hours    ),
+    'unsafe_shutdowns'      : ('Unsafe Shutdown'   , emit_count           ), # Put it together with power cycles
+    'media_errors'          : ('Media Errors'      , emit_error           ), # Uncorrectable events
+    'num_err_log_entries'   : ('Error Logs'        , emit_error           ),
+    'warning_temp_time'     : ('Warning Temp'      , emit_duration_minutes),
+    'critical_comp_time'    : ('Critical Temp'     , emit_duration_minutes),
+    'temperature_sensor_1'  : ('Sensor 1'          , emit_temperature     ), # Not always present
+    'temperature_sensor_2'  : ('Sensor 2'          , emit_temperature     ), # Not always present
+    'temperature_sensor_3'  : ('Sensor 3'          , emit_temperature     ), # Not always present
+    'temperature_sensor_4'  : ('Sensor 4'          , emit_temperature     ), # Not always present
+    'temperature_sensor_5'  : ('Sensor 5'          , emit_temperature     ), # Not always present
+    'temperature_sensor_6'  : ('Sensor 6'          , emit_temperature     ), # Not always present
+    'temperature_sensor_7'  : ('Sensor 7'          , emit_temperature     ), # Not always present
+    'temperature_sensor_8'  : ('Sensor 8'          , emit_temperature     ), # Not always present
+    'thm_temp1_trans_count' : ('Thermal Limit 1'   , emit_error           ),
+    'thm_temp2_trans_count' : ('Thermal Limit 2'   , emit_error           ),
+    'thm_temp1_total_time'  : ('Thermal Limit 1'   , emit_duration_seconds),
+    'thm_temp2_total_time'  : ('Thermal Limit 2'   , emit_duration_seconds),
+    'wctemp'                : ('Warning Composite' , emit_temperature     ), # From controller ID
+    'cctemp'                : ('Critical Composite', emit_temperature     ), # From controller ID
   }
 
   out = subprocess.Popen(['nvme', cmd, '-o', 'json', '/dev/' + dev], stdout = subprocess.PIPE).communicate()[0]
   j = json.loads(out)
   # Assuming python 2.x
-  for key, val in j.iteritems():
+  for key, val in list(j.items()):
     try:
-      a = names[key]
-      f = funcs[key]
-      if a is not None and f is not None:
-        f(vl, a, val)
+      a, f = properties[key]
+      f(vl, a, val)
     except KeyError as e:
       #print e
       pass
 
+# Slightly different rules here
+def process_ns(vl, ns):
+  out = subprocess.Popen(['nvme', 'id-ns', '-o', 'json', '/dev/' + ns], stdout = subprocess.PIPE).communicate()[0]
+  j = json.loads(out)
+  size, cap, used = [j[k] for k in ['nsze', 'ncap', 'nuse']]
+  emit_df(vl, 'free', cap - used)
+  emit_df(vl, 'used', used)
+  emit_df(vl, 'reserved', size - cap)
 
 def read(data = None):
   vl = collectd.Values(type = 'gauge')
@@ -147,6 +118,12 @@ def read(data = None):
       vl.plugin_instance = dev
       process_cmd(vl, dev, 'id-ctrl')
       process_cmd(vl, dev, 'smart-log')
+
+  rens = re.compile('nvme[0-9]+n[0-9]+$')
+  for ns in os.listdir('/dev'):
+    if rens.match(ns):
+      vl.plugin_instance = ns
+      process_ns(vl, ns)
 
 
 collectd.register_read(read)
