244af5750ba18e2d278156cc15b14d289d29c8e8
dword1511 <zhangchi866@gmail.com>
2020-05-11 00:42:31 -0700

    htu21d: read sn

--- envsensor/htu21d.py
+++ envsensor/htu21d.py
@@ -20,31 +20,63 @@ class HTU21D:
   CMD_READ_USER_REG   = 0xe7
   CMD_RESET           = 0xfe
 
+  # Stuff hidden in the app note (HPC207_0 October 2012)
+  CMD_READ_SN_1       = 0xfa
+  CMD_READ_SN_2       = 0x0f
+  CMD_READ_SN_3       = 0xfc
+  CMD_READ_SN_4       = 0xc9
+  SN_FIXED_FIELD      = 0x4854000000003200
+  SN_FIXED_FIELD_MASK = 0xffffff000000ff00
+
   def __init__(self, busno, address = I2C_ADDR):
     self.busno = busno
     self.bus = SMBus(busno)
     self.address = address
 
+  def read_serial(self):
+    i2c_rdwr_write(self.bus, self.address, [self.CMD_READ_SN_1, self.CMD_READ_SN_2])
+    response = i2c_rdwr_read(self.bus, self.address, 8)
+    snb3, crc_snb3, snb2, crc_snb2, snb1, crc_snb1, snb0, crc_snb0 = response
+    HTU21D.crc_check([snb3], crc_snb3)
+    HTU21D.crc_check([snb2], crc_snb2)
+    HTU21D.crc_check([snb1], crc_snb1)
+    HTU21D.crc_check([snb0], crc_snb0)
+    snb = (snb3 << 24) | (snb2 << 16) | (snb1 << 8) | snb0
+
+    i2c_rdwr_write(self.bus, self.address, [self.CMD_READ_SN_3, self.CMD_READ_SN_4])
+    response = i2c_rdwr_read(self.bus, self.address, 6)
+    snc1, snc0, crc_snc, sna1, sna0, crc_sna = response
+    HTU21D.crc_check([snc1, snc0], crc_snc)
+    HTU21D.crc_check([sna1, sna0], crc_sna)
+    snc = (snc1 << 8) | snc0
+    sna = (sna1 << 8) | sna0
+
+    sn = (sna << 48) | (snb << 16) | snc
+    if sn & self.SN_FIXED_FIELD_MASK != self.SN_FIXED_FIELD:
+      raise ValueError('S/N {:016x} is invalid for HTU21D'.format(sn))
+    return sn
+
   def read_temperature(self):
     i2c_rdwr_write(self.bus, self.address, [self.CMD_TRIG_TEMP_NHM])
     time.sleep(0.06) # actual: 50 ms max
     msb, lsb, crc = i2c_rdwr_read(self.bus, self.address, 3)
-    crc_computed = HTU21D.compute_crc([msb, lsb])
-    if crc != crc_computed:
-      raise IOError('expected CRC 0x{:02x}, got 0x{:02x}'.format(crc_computed, crc))
+    HTU21D.crc_check([msb, lsb], crc)
     return -46.85 + 175.72 * ((msb << 8) + lsb) / float(1 << 16)
 
   def read_humidity(self):
     i2c_rdwr_write(self.bus, self.address, [self.CMD_TRIG_HUMID_NHM])
     time.sleep(0.02) # actual: 16 ms max
     msb, lsb, crc = i2c_rdwr_read(self.bus, self.address, 3)
-    crc_computed = HTU21D.compute_crc([msb, lsb])
-    if crc != crc_computed:
-      raise IOError('expected CRC 0x{:02x}, got 0x{:02x}'.format(crc_computed, crc))
+    HTU21D.crc_check([msb, lsb], crc)
     return -6 + 125 * ((msb << 8) + lsb) / float(1 << 16)
 
   def reset(self):
     i2c_rdwr_write(self.bus, self.address, [self.CMD_RESET])
+
+  def crc_check(bytes, expected):
+    computed = HTU21D.compute_crc(bytes)
+    if computed != expected:
+      raise IOError('Computed CRC 0x{:02x}, expected 0x{:02x}'.format(computed, expected))
 
   def compute_crc(bytes):
     poly = 0x131
@@ -99,8 +131,9 @@ def init():
     try:
       sensor = HTU21D(bus)
       sensor.reset()
+      sn = sensor.read_serial()
       sensors.append(sensor)
-      logi('Initialized sensor on i2c-{}'.format(bus))
+      logi('Initialized sensor on i2c-{}, S/N: {:016x}'.format(bus, sn))
     except:
       loge('Failed to init sensor on i2c-{}'.format(bus))
 
