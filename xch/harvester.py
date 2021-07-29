5013cd32e32e68d5b351ca1a538e58661fcbc336
dword1511 <zhangchi866@gmail.com>
2021-07-28 22:49:03 -0700

    xch.harvester: add script for monitoring chia harvester

---
+++ xch/harvester.py
@@ -0,0 +1,171 @@
+"""Monitors Chia harvesters.
+
+Depends on: pygtail
+"""
+
+import os
+import re
+import tempfile
+
+import collectd
+
+from pygtail.core import Pygtail
+
+_PATTERN_PLOT_INFO = re.compile(
+    r'Loaded a total of ([0-9]+) plots of size ([0-9]+\.[0-9]+) TiB, '
+    r'in ([0-9]+\.[0-9]+) seconds')
+
+# Although we can get a more up-to-date plot count here, we won't be able to
+# update total plot size.
+_PATTERN_PROOF_INFO = re.compile(
+    r'([0-9]+) plots were eligible for farming ([0-9a-f]+)... '
+    r'Found ([0-9]+) proofs. '
+    r'Time: ([0-9]+\.[0-9]+) s. ')
+
+_last_plot_count = 0
+_last_plot_size_tib = 0.
+_last_plot_scan_time = 0.
+_last_plot_scan_time_max = 0.
+
+_log_dir = None
+_log = None
+
+
+def _update_last_plot_info(lines):
+    global _last_plot_count, _last_plot_size_tib, _last_plot_scan_time, _last_plot_scan_time_max
+
+    for line in lines:
+        plot_info_matches = _PATTERN_PLOT_INFO.search(line)
+        if plot_info_matches:
+            _last_plot_count = int(plot_info_matches.group(1))
+            _last_plot_size_tib = float(plot_info_matches.group(2))
+            _last_plot_scan_time = float(plot_info_matches.group(3))
+            _last_plot_scan_time_max = max(_last_plot_scan_time_max,
+                                           _last_plot_scan_time)
+
+
+def config(config_in):
+    """Parses additional config.
+
+    Config example:
+
+    Import "xch.harvester"
+    <Module "xch.harvester">
+      LogDir "/home/somebody/.chia/mainnet/log/"
+    </Module>
+    """
+    global _log_dir
+
+    for node in config_in.children:
+        key = node.key.lower()
+        val = node.values
+
+        if key == 'LogDir'.lower():
+            assert len(val) == 1
+            _log_dir = val[0]
+        else:
+            collectd.warning('Skipping unknown config key "{}"'.format(key))
+
+
+def init():
+    """Opens log files for read.
+
+    Pygtail can handle rotated logs (debug.log.[0-9]) from chia.
+    """
+    global _log
+
+    print(_log_dir)
+    if not _log_dir:
+        raise RuntimeError('LogDir must be specified')
+
+    log_path = f'{_log_dir}/debug.log'
+    fd, offset_path = tempfile.mkstemp(prefix='collectd_xch_harvester',
+                                       suffix='offset')
+    os.close(fd)
+    _log = Pygtail(filename=log_path, offset_file=offset_path)
+    _update_last_plot_info(_log.readlines())
+
+    return True
+
+
+def read(_=None):
+    global _last_plot_scan_time
+
+    values = collectd.Values(plugin='xch')
+    values.plugin_instance = 'harvester'
+
+    lines = _log.readlines()
+
+    _update_last_plot_info(lines)
+    try:
+        values.dispatch(type='gauge',
+                        type_instance='Plot count',
+                        values=[_last_plot_count])
+        values.dispatch(type='bytes',
+                        type_instance='Total plot size',
+                        values=[round(_last_plot_size_tib * (1024**4))])
+        values.dispatch(type='duration',
+                        type_instance='Plot scan last',
+                        values=[_last_plot_scan_time])
+        values.dispatch(type='duration',
+                        type_instance='Plot scan max',
+                        values=[_last_plot_scan_time_max])
+        _last_plot_scan_time = 0.
+    except ValueError as err:
+        collectd.error(err)
+
+    eligible_plot_count = 0
+    blocks = set()
+    proof_count = 0
+    time_total = 0.
+    time_max = 0.
+    time_min = None
+    for line in lines:
+        proof_info_matches = _PATTERN_PROOF_INFO.search(line)
+        if proof_info_matches:
+            try:
+                eligible_plot_count += int(proof_info_matches.group(1))
+            except ValueError as err:
+                collectd.error(err)
+
+            blocks.add(proof_info_matches.group(2))
+
+            try:
+                proof_count += int(proof_info_matches.group(3))
+            except ValueError as err:
+                collectd.error(err)
+
+            try:
+                time = float(proof_info_matches.group(4))
+                time_total += time
+                time_max = max(time_max, time)
+                if not time_min:
+                    time_min = time
+                time_min = min(time_min, time)
+            except ValueError as err:
+                collectd.error(err)
+
+    values.dispatch(type='gauge',
+                    type_instance='Eligible plot count',
+                    values=[eligible_plot_count])
+    values.dispatch(type='gauge',
+                    type_instance='Block count',
+                    values=[len(blocks)])
+    values.dispatch(type='gauge',
+                    type_instance='Proof count',
+                    values=[proof_count])
+    if proof_count != 0:
+        values.dispatch(type='duration',
+                        type_instance='Proof average',
+                        values=[time_total / proof_count])
+        values.dispatch(type='duration',
+                        type_instance='Proof max',
+                        values=[time_max])
+        values.dispatch(type='duration',
+                        type_instance='Proof min',
+                        values=[time_min])
+
+
+collectd.register_config(config)
+collectd.register_init(init)
+collectd.register_read(read)
